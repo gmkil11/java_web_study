e6bdc49f5335ecaa2a3272bcec1f892c245c3156
gmkil11 <mkkil4451@gmail.com>
2023-10-13 14:14:32 +0900

    day04

--- day04/src/main/java/servlets/HelloServlet.java
+++ day04/src/main/java/servlets/HelloServlet.java
@@ -9,7 +9,19 @@ import java.io.IOException;
 
 public class HelloServlet extends HttpServlet {
     @Override
+    public void destroy() {
+        System.out.println("destroy!!!");
+    }
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("init!!!");
+    }
+
+    //    private String abc; -> 다음과 같은 식은 간섭이 발생할 수 있기 때문에 지양한다.
+    @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("안녕하세요...");
+//        System.out.println("안녕하세요...");
+        System.out.println("doGet!!!");
     }
 }
